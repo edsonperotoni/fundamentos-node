6b2a5bc5a39206e381ceddafede4904572e6cdb2
Edson Perotoni <perotoni@gmail.com>
2020-05-09 17:56:30 -0300

    create and list transactions

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,9 +1,21 @@
 import Transaction from '../models/Transaction';
 
+interface TransactionDTO {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
   total: number;
+}
+interface Report {
+  transactions: Transaction[];
+  balance: Balance;
 }
 
 class TransactionsRepository {
@@ -15,14 +27,54 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+
+    return this.transactions.reduce(
+      function (totals: Balance, obj: Transaction): Balance {
+        if (obj.type === 'income') {
+          totals.income += obj.value;
+        }
+        if (obj.type === 'outcome') {
+          totals.outcome += obj.value;
+        }
+        totals.total = totals.income - totals.outcome;
+        return totals;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: TransactionDTO): Transaction {
     // TODO
+    if (!(type === 'income' || type === 'outcome')) {
+      throw Error("The type of transaction must be 'income' or 'outcome'");
+    }
+
+    if (type === 'outcome') {
+      const currentBalance = this.getBalance().total;
+      if (currentBalance < value) {
+        throw Error('There is not enough balance');
+      }
+    }
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    return transaction;
+  }
+
+  public report(): Report {
+    // TODO
+    return {
+      transactions: this.all(),
+      balance: this.getBalance(),
+    };
   }
 }
 
--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,26 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
+
+interface Request {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
 
 transactionRouter.get('/', (request, response) => {
   try {
     // TODO
+
+    const report = transactionsRepository.report();
+    return response.status(200).json(report);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -18,6 +29,18 @@ transactionRouter.get('/', (request, response) => {
 transactionRouter.post('/', (request, response) => {
   try {
     // TODO
+
+    const { title, value, type } = request.body;
+
+    const transactionRequest: Request = { title, value, type };
+
+    const createTransactionService = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransactionService.execute(transactionRequest);
+
+    return response.status(200).json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
